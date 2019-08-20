724b2ab976b66e59599a46284df2728d7c23f8fd
Solene <solene@seald.io>
2019-08-20 14:15:03 +0200

    Changement scrap 1

--- scrapper_1/scrapper.py
+++ scrapper_1/scrapper.py
@@ -5,22 +5,22 @@ import html
 import csv
 import numpy as np
 import os
-
-# Nom du fichier contenant les liens
-filenameLinks = 'liens_france.npy'
+import pandas as pd
 
 # Nom du fichier de sauvegarde
-saveAs = 'contacts_avocats_barreau_paris.csv'
+saveAs = 'contacts_avocats_barreau_paris.xslx'
 
-linksObject = np.load(filenameLinks)
+# Nom du fichier contenant les liens
+script_dir = os.path.dirname(__file__)
+filenameLinks = 'liens_france.npy'
+file_path = os.path.join(script_dir, filenameLinks)
+
+
+
+linksObject = np.load(file_path)
 # Transforme notre object en liste
 links = list(linksObject)
 
-# Ces liens ne fonctionnent pas
-del links[281]
-del links[575]
-del links[625]
-del links[454]
 
 requests_headers = {'User-Agent':"(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"}
 
@@ -34,31 +34,34 @@ email = []
 n = 0
 for i in links :
     n += 1
-    request = requests.get(i, headers = requests_headers, timeout = 10)
-    bodyString = request.text
-    bodyString = html.unescape(bodyString)
-    bodyString = bodyString.replace('\n', '')
-    bodyString = bodyString.replace('\t', '')
-    bodyString = bodyString.replace('\r', '')
+    try :
+        request = requests.get(i, headers = requests_headers, timeout = 10)
+        bodyString = request.text
+        bodyString = html.unescape(bodyString)
+        bodyString = bodyString.replace('\n', '')
+        bodyString = bodyString.replace('\t', '')
+        bodyString = bodyString.replace('\r', '')
+        print(n)
 
-    companyResult = re.findall('<span itemprop="itemreviewed" ><em>(.+?(?=</em></span>))', bodyString)
-    company.extend(companyResult)
+        companyResult = re.findall('<span itemprop="itemreviewed" ><em>(.+?(?=</em></span>))', bodyString)
+        company.extend(companyResult)
 
-    addressResult = re.findall('</div><div class="Information"><span>(.+?(?=</span></div>))', bodyString)
-    address.append(addressResult[0])
-    city.append(addressResult[1])
-    country.append(addressResult[2])
-    number.append(addressResult[3])
+        addressResult = re.findall('</div><div class="Information"><span>(.+?(?=</span></div>))', bodyString)
+        address.append(addressResult[0])
+        city.append(addressResult[1])
+        country.append(addressResult[2])
+        number.append(addressResult[3])
 
-    emailResult = re.findall('E-mail</span></div><div class="Colon"><span>:</span></div><div class="Information"><a href="mailto:(.+?(?=">))', bodyString)
-    email.extend(emailResult)
+        emailResult = re.findall('E-mail</span></div><div class="Colon"><span>:</span></div><div class="Information"><a href="mailto:(.+?(?=">))', bodyString)
+        email.extend(emailResult)
+    except :
+        print('lien ', n, ' ne fonctionne pas')
+        pass
 
-result = [company, email, address, city, country , number]
 
-script_dir = os.path.dirname(__file__)
+
 file_path = os.path.join(script_dir, saveAs)
-with open(file_path, "w", encoding = 'utf-8') as outfile :
-    data = csv.writer(outfile, delimiter = ';', lineterminator = '\n')
-    data.writerow(['Nom', 'Email', 'Adresse', 'Ville', 'Pays', 'Numéro'])
-    for row in zip(*result) :
-        data.writerow(row)
+
+df = pd.DataFrame.from_dict({'Nom':company,'Email':email,'Adresse':address,'Numéro':number, 'Ville':city, 'Pays':country})
+df.to_excel(file_path, header=True, index=False)
+
