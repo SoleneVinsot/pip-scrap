dbf619e8b44fd4a5b03017db250f8b3bfffff399
Solene Vinsot <solene.vinsot@yahoo.fr>
2019-10-01 19:04:31 +0200

    g

--- Cours_M2/Folium.py
+++ Cours_M2/Folium.py
@@ -1,8 +1,9 @@
 import folium
 import csv
 import pdb
+import json
 
-#carte de paris
+"""#carte de paris
 paris = folium.Map(location = [48.856578, 2.351828], zoom_start = 12.2)
 
 lat=48.8581
@@ -20,7 +21,7 @@ with open('Cours_python_M2.csv', newline='') as csvfile:
             lon=float(row[2])
             folium.Marker([lat,lon],popup=row[0]).add_to(paris)
 
-        
+
 
 
 
@@ -28,14 +29,10 @@ with open('Cours_python_M2.csv', newline='') as csvfile:
 #folium.RegularPolygonMarker([lat,lon],color='none',fill_color='red',number_of_sides=4,radius=5,popup='Tour Eiffel').add_to(paris)
 
 paris.save('Carte_de_paris.html')
-
-
-
-
-
-"""with open('Cours_python_M2.csv', newline='') as csvfile:
-  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-  for row in spamreader:
-    print(', '.join(row))
 """
+
+with open('DPEstado_do_RioFinal.json','r') as output:
+    Base_geo=json.load(output)
+ 
+
 
